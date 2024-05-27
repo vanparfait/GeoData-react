84084245e53eba2fa48d2817373ff02f46cbcdfd
vanparfait <nguemechiaparfait@gmail.com>
2024-05-27 12:03:45 +0100

    appel de l'API

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ function App() {
       console.log(response);
       const datas = await response.json();
       console.log(datas);
+
+      datas.sort((a, b) => {
+        if (a.name.common < b.name.common) return -1;
+        else if (a.name.common > b.name.common) return -1;
+        else return 0;
+      });
+
       setCountries(datas);
     };
 
@@ -21,7 +28,10 @@ function App() {
     <div className="min-h-screen bg-slate-800">
       <div className="max-w-7xl mx-auto py-20 px-4">
         <h1 className="text-gray-50 text-4xl">Europe countries Data</h1>
-        <h1>{countries[0]}</h1>
+        {countries &&
+          countries.map((country) => (
+            <h1 key={country.ccn3}>{country.cca2}</h1>
+          ))}
         <p className="text-gray-100 text-xl nb-8">
           Click on a card to reveal a country's information
         </p>
